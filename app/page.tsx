62e90aa23a6b4801053e6bd62d6f2dcade4ee4b4
bhumika <bhumika@celominds.com>
2023-02-18 23:18:05 +0530

    added metadata list

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
+import fs from "fs";
+
+const getPostMetaData = () => {
+  const folder = "posts/";
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) => file.endsWith(".md"));
+  const slugs = markdownPosts.map((file) => file.replace(".md", ""));
+  return slugs;
+};
 const Homepage = () => {
-  return <h1></h1>;
+  const postMetaData = getPostMetaData();
+  const postPreviews = postMetaData.map((slug) => (
+    <div>
+      <h2>{slug}</h2>
+    </div>
+  ));
+  return <div>{postPreviews}</div>;
 };
 
 export default Homepage;
